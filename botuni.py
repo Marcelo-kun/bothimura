3d92afdb5dd0d98bf43269a2860dc3919bdee524
unknown <marcehimura2@gmail.com>
2022-08-12 15:31:43 -0400

    botuni

---
+++ botuni.py
@@ -0,0 +1,32 @@
+from crypt import methods
+from requests import post, request
+import telebot
+from flask import Flask, request
+import os
+
+API_TOKEN  = '5526189505:AAGV3T6-SIgRa_mo1JrZsMkmdV5wjakklLM'
+
+bot = telebot.TeleBot(API_TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def send_welkome(message):
+    bot.reply_to(message, "Bienvenido/a al Bot")
+
+@bot.message_handler(commands=['Hola'])
+def send_welkome(message):
+    bot.reply_to(message, "Hola, soy un ChatBot informativo de la Universidad Gran Asuncion, en que puedo ayudarte?")
+
+@server.route('/' + API_TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return "!", 200
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://bothimura.herokuapp.com/' + API_TOKEN)
+    return "!", 200
+
+if __name__ + '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
