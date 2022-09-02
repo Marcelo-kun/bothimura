d6a2c36badcd1ce678407d3a5e3203e54d374f7a
unknown <marcehimura2@gmail.com>
2022-09-01 21:59:39 -0400

    edicion boton new

--- botuni.py
+++ botuni.py
@@ -1,22 +1,43 @@
+import types
 from requests import post, request
 import telebot
 from flask import Flask, request
 import os
 from telebot.types import InlineKeyboardMarkup
 from telebot.types import InlineKeyboardButton
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
         
-
 
 API_TOKEN  = '5526189505:AAGV3T6-SIgRa_mo1JrZsMkmdV5wjakklLM'
 
 bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
 
+button1 = KeyboardButton('Hello')
+button2 = KeyboardButton('Youtube')
+keyboard1=ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button1).add(button2)
+
+button3 = KeyboardButton('Hello', request_contact=True)
+button4 = KeyboardButton('Youtube', request_contact=True)
+keyboard2=ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button3).add(button4)
+
 @bot.message_handler(commands=['start'])
 def send_welkome(message):
     bot.reply_to(message, "Hola, soy un 🤖ChatBot informativo de la Universidad Gran Asuncion")
+
+@bot.message_handler(commands=['info'])
+def info(message):
+    message.reply('Say something about you:', reply_markup=keyboard2)
+
+@bot.message_handler()
+def kb_answer(message):
+    if message.text == 'Hello':
+        message.answer('Hola, sigue asi')
+    elif message.text == 'Youtube':
+        message.answer('mira mas tutoriales')
+    else:
+        message.answer(f' Your message is: {message.text}')
 
 
 @bot.message_handler(commands=['botones'])
@@ -27,7 +48,7 @@ def cmd_botones(message):
     b2 = InlineKeyboardButton("UNIGRAN WEB", url="https://www.unigran.edu.py/")
     b3 = InlineKeyboardButton("UNIGRAN FACEBOOK", url="https://www.facebook.com/unigranparaguay?_rdc=1&_rdr")
     markup.add(b1, b2, b3)
-    bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓", reply_markup=markup)
+    bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓Haz click en el botón", reply_markup=markup)
 
 @server.route('/' + API_TOKEN, methods=['POST'])
 def getMessage():
