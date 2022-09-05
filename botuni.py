4c598ae9aeb9a8fc1c9b7c017e01a513897779cb
unknown <marcehimura2@gmail.com>
2022-09-05 16:09:01 -0400

    edicion boton new18

--- botuni.py
+++ botuni.py
@@ -1,14 +1,8 @@
-from ast import pattern
 from asyncore import dispatcher
-from cgitb import text
-from email.message import Message
-from smtplib import _Reply
 from tkinter import Button
-from click import command
 from requests import request
-from setuptools import Command
 import telebot
-from flask import Flask, request
+from flask import Flask, message_flashed, request
 import os
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
@@ -22,50 +16,10 @@ API_TOKEN  = '5526189505:AAGV3T6-SIgRa_mo1JrZsMkmdV5wjakklLM'
 bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
 
-button1 = KeyboardButton('Hello')
-button2 = KeyboardButton('Youtube')
-keyboard1=ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(button1, button2)
-
-button3 = KeyboardButton('Hello' , request_contact=True)
-button4 = KeyboardButton('Youtube', request_contact=True)
-keyboard2=ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(button3, button4)
-
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message=="Resultado 1", reply_markup=keyboard1)
-
-@bot.message_handler(commands=['info'])
-def info(message):
-    bot.reply_to(message=="Resultado 2 ", reply_markup=keyboard2)
-
-
-"""@bot.message_handler(commands=['start'])
 def send_welkome(message):
-    bot.reply_to(message, "Hola, soy un 🤖ChatBot informativo de la Universidad Gran Asuncion")"""
+    bot.reply_to(message, "Hola, soy un 🤖ChatBot informativo de la Universidad Gran Asuncion")
     
-
-
-"""def comand_callback_handler(update, context):
-    query = update.callback_query
-    query.answer()
-
-    query.edit_message_text(
-        text='Te envio un texto'
-    )
-    return INPUT_TEXT
-
-
-    button1 = InlineKeyboardButton(
-        text = 'Sobre el autor',
-        url = 'https://www.youtube.com/watch?v=DBoWGTO4TaU&t=824s'
-    )
-
-    update.message.reply.text(
-        text='Haz click en un boton',
-        reply_markup=InlineKeyboardMarkup([
-            [button1]
-        ])
-    )"""
 
     
 
@@ -79,11 +33,21 @@ def cmd_botones(message):
     markup.add(b1, b2, b3)
     bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓Haz click en el botón", reply_markup=markup)
 
-"""@bot.message_handler(commands=['consulta'])
-def consulta_message(message):
+@bot.message_handler(commands=['boton'])
+def cmd_boton(message):
+    """Muestra un mensaje con botones inline (a continuacion del mensaje)"""
+    boton_markup = ReplyKeyboardMarkup(row_width = 2)
+    b4 = KeyboardButton("UGA Radio", message,"http://ugaradio.com.py/")
+    b5 = KeyboardButton("UNIGRAN WEB", url="https://www.unigran.edu.py/")
+    b6 = KeyboardButton("UNIGRAN FACEBOOK", url="https://www.facebook.com/unigranparaguay?_rdc=1&_rdr")
+    boton_markup.add(b4, b5, b6)
+    bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓Haz click en el botón", reply_markup=boton_markup)
+
+@bot.message_handler(commands=['consulta'])
+def consu_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
     keyboard.row = ('Carreras')
-    bot.send_message(message.chat.id, 'Consulta1', reply_markup=keyboard)"""
+    bot.send_message(message.chat.id, 'Consulta1', reply_markup=keyboard)
 
 
 
