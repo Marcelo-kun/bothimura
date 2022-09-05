c2a21317e94ba7453860401c33526a0432cc1b41
unknown <marcehimura2@gmail.com>
2022-09-04 22:21:57 -0400

    edicion boton new9

--- botuni.py
+++ botuni.py
@@ -14,22 +14,22 @@ API_TOKEN  = '5526189505:AAGV3T6-SIgRa_mo1JrZsMkmdV5wjakklLM'
 bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
 
+@bot.message_handler(commands=['botones'])
+def cmd_botones(message):
+    """Muestra un mensaje con botones inline (a continuacion del mensaje)"""
+    markup = InlineKeyboardMarkup(row_width = 2)
+    b1 = KeyboardButton("UGA Radio", text="http://ugaradio.com.py/")
+    b2 = InlineKeyboardButton("UNIGRAN WEB", url="https://www.unigran.edu.py/")
+    b3 = InlineKeyboardButton("UNIGRAN FACEBOOK", url="https://www.facebook.com/unigranparaguay?_rdc=1&_rdr")
+    markup.add(b1, b2, b3)
+    bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓Haz click en el botón", reply_markup=markup)
+
 
 @bot.message_handler(commands=['start'])
 def send_welkome(message):
     bot.reply_to(message, "Hola, soy un 🤖ChatBot informativo de la Universidad Gran Asuncion")
 
 
-
-@bot.message_handler(commands=['botones'])
-def cmd_botones(message):
-    """Muestra un mensaje con botones inline (a continuacion del mensaje)"""
-    markup = InlineKeyboardMarkup(row_width = 2)
-    b1 = InlineKeyboardButton("UGA Radio", text="http://ugaradio.com.py/")
-    b2 = InlineKeyboardButton("UNIGRAN WEB", url="https://www.unigran.edu.py/")
-    b3 = InlineKeyboardButton("UNIGRAN FACEBOOK", url="https://www.facebook.com/unigranparaguay?_rdc=1&_rdr")
-    markup.add(b1, b2, b3)
-    bot.send_message(message.chat.id, "Enlaces que pueden intereasarte 🎓Haz click en el botón", reply_markup=markup)
 
 @server.route('/' + API_TOKEN, methods=['POST'])
 def getMessage():
