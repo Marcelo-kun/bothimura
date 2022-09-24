17f4ab01e876575d6760f73d315816a50306947f
unknown <marcehimura2@gmail.com>
2022-09-24 16:02:20 -0400

    boton modificadoo0022

--- botuni.py
+++ botuni.py
@@ -53,6 +53,18 @@ def guardar_datos_usuario(message):
         
         if usuarios[message.chat.id]["curso"] == "1":
             usuarios[message.chat.id]["cuota"] == "500.000Gs"
+            ######################
+            texto = 'Datos introducidos:\n'
+            texto+= f'<code>Nombre.:</code> {usuarios[message.chat.id]["nombre"]}\n'
+            texto+= f'<code>Curso..:</code> {usuarios[message.chat.id]["curso"]}\n'
+            texto+= f'<code>Carrera:</code> {usuarios[message.chat.id]["carrera"]}\n'
+            texto+= f'<code>Carrera:</code> {usuarios[message.chat.id]["cuota"]}\n'
+            markup = ReplyKeyboardRemove()
+            bot.send_message(message.chat.id, texto, parse_mode="html", reply_markup=markup)
+            print(usuarios) #para que se vea en terminal
+            del usuarios[message.chat.id] #se elimina los datos del diccinario
+            ######################
+
         else: 
             msg = bot.send_message(message.chat.id, "Error: Curso no valido.\n Pulsa un boton")
     else: 
@@ -67,15 +79,7 @@ def guardar_datos_usuario(message):
 
             #usuarios[message.chat.id]["curso"] = message.text
             #se crea una variable tipo string donde se guarda una cadena de texto en la que se indica los datos introducidos formateando la salida
-    texto = 'Datos introducidos:\n'
-    texto+= f'<code>Nombre.:</code> {usuarios[message.chat.id]["nombre"]}\n'
-    texto+= f'<code>Curso..:</code> {usuarios[message.chat.id]["curso"]}\n'
-    texto+= f'<code>Carrera:</code> {usuarios[message.chat.id]["carrera"]}\n'
-    texto+= f'<code>Carrera:</code> {usuarios[message.chat.id]["cuota"]}\n'
-    markup = ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, texto, parse_mode="html", reply_markup=markup)
-    print(usuarios) #para que se vea en terminal
-    del usuarios[message.chat.id] #se elimina los datos del diccinario
+    
 
 #codigo con botones################################
 """def preguntar_curso(message): 
