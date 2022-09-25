62f38ca88931d669d520a6fc6dfcd9234f70bf48
unknown <marcehimura2@gmail.com>
2022-09-25 16:53:32 -0400

    errores listados probando56

--- botuni.py
+++ botuni.py
@@ -62,7 +62,7 @@ def guardar_datos_usuario(message):
     if message.text != "Ing. Informatica" and message.text != "Ing. Comercial" and message.text != "Ing. en Marketing y Publicidad" and message.text != "Lic. en Ciencias Contables" and message.text != "Lic. en Ciencias de la Educación" and message.text != "Lic. en Enfermería" and message.text != "Lic. en Psicología" and message.text != "Derecho":
         msg = bot.send_message(message.chat.id, "Error: Curso no valido.\n Pulsa un boton")
         #se vuelve a ejecutar la funcion
-        bot.register_next_step_handler(msg, preguntar_curso)
+        bot.register_next_step_handler(msg, preguntar_carrera)
     
     elif usuarios[message.chat.id]["carrera"] == "Ing. Informática":
         
