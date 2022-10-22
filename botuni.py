fdecaacf5b22791a86bb50b5345cdd79358becdc
unknown <marcehimura2@gmail.com>
2022-10-22 17:10:34 -0300

    modificadoscondiicionaal

--- botuni.py
+++ botuni.py
@@ -55,7 +55,7 @@ def preguntar_carrera(message):
 def preguntar_curso(message):#esta funcion contiene la respuesta anterior    
     usuarios[message.chat.id]["carrera"] = "" #se guarda curso dentro del diccionario vacio
     usuarios[message.chat.id]["carrera"] = message.text #se guarda curso dentro del diccionario vacio
-    if usuarios[message.chat.id]["carrera"] != "Ing. Informática" or usuarios[message.chat.id]["carrera"] != "Ing. Comercial" or usuarios[message.chat.id]["carrera"] != "Ing. en Marketing y Publicidad" or usuarios[message.chat.id]["carrera"] != "Lic. en Ciencias Contables" or usuarios[message.chat.id]["carrera"] != "Lic. en Ciencias de la Educación" or usuarios[message.chat.id]["carrera"] != "Lic. en Enfermería" or usuarios[message.chat.id]["carrera"] != "Lic. en Psicología" or usuarios[message.chat.id]["carrera"] != "Derecho":
+    if usuarios[message.chat.id]["carrera"] != "Ing. Informática" """or usuarios[message.chat.id]["carrera"] != "Ing. Comercial" or usuarios[message.chat.id]["carrera"] != "Ing. en Marketing y Publicidad" or usuarios[message.chat.id]["carrera"] != "Lic. en Ciencias Contables" or usuarios[message.chat.id]["carrera"] != "Lic. en Ciencias de la Educación" or usuarios[message.chat.id]["carrera"] != "Lic. en Enfermería" or usuarios[message.chat.id]["carrera"] != "Lic. en Psicología" or usuarios[message.chat.id]["carrera"] != "Derecho""":
         msg = bot.send_message(message.chat.id, "Error: Carrera no valida.\n Pulsa un boton")
         bot.register_next_step_handler(msg, preguntar_carrera)
     else:
